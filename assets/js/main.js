9d260f46259c72502376270c2c6fd9a50e8cea28
KelvenH <kelvenhughes@me.com>
2021-10-17 17:04:40 +0000

    Added object buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,32 @@ audio1.onplaying = function() {
 audio1.onpause = function() {
   isPlaying = false;
 };
+
+
+
+function phoneOn() {
+  //close modal
+  var modal = document.getElementById('Modal-office');
+  modal.classList.remove('show');
+  modal.setAttribute('aria-hidden', 'true');
+  modal.setAttribute('style', 'display: none');
+  //document.getElementsByClassName('backdrop').style.display = 'none';
+  
+  //remove black image overlay
+  document.getElementById("s3-img-overlay").style.backgroundColor = "rgba(0, 0, 0, 0)";
+  
+  //activate torchlight effect over main image
+  document.getElementById("s3-main-img").classList.add("torch");
+
+}
+
+function update(e){
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
+
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
