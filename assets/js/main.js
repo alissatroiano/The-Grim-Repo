cc6acebb4b9e04be5b1a41e9bc4d675194eebb0b
KelvenH <kelvenhughes@me.com>
2021-10-17 12:02:10 +0000

    added audio toggle and some adjustments to templates css

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,19 @@ let test = "test";
 // main
 
 console.log(test);
+
+// play background audio
+
+var audio1 = document.getElementById("audio1");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? audio1.pause() : audio1.play();
+};
+
+audio1.onplaying = function() {
+  isPlaying = true;
+};
+audio1.onpause = function() {
+  isPlaying = false;
+};
